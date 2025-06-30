5746edfdd9b09ae2c13425b9d7d7839f2320e30e
surya <surya.a@aol.com>
2025-06-30 18:54:41 +0700

    Update committed at 2025-06-30 18:54:41 by surya > modified: src/

--- src/flazzy/converter.py
+++ src/flazzy/converter.py
@@ -2,13 +2,6 @@ def convert_currency(amount: float, from_curr: str, to_curr: str, rates: dict):
     return amount * (rates[to_curr] / rates[from_curr])
 
 
-def swap_currency(amount: float, from_curr: str, to_curr: str, rates: dict):
-    rate = convert_currency(1.0, to_curr, from_curr, rates)
-    if rate == 0:
-        raise ValueError("Cannot divide by zero.")
-    swapped_result = amount / rate
-    return swapped_result
-
 
 def generate_exchange_pairs(data: dict):
     base = data["base"]
