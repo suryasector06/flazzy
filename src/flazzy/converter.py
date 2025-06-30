01a63a90aa6196eb36494a4633dbaeadc5588b12
surya <surya.a@aol.com>
2025-06-30 18:55:31 +0700

    Update committed at 2025-06-30 18:55:31 by surya > modified: src/

--- src/flazzy/converter.py
+++ src/flazzy/converter.py
@@ -2,7 +2,6 @@ def convert_currency(amount: float, from_curr: str, to_curr: str, rates: dict):
     return amount * (rates[to_curr] / rates[from_curr])
 
 
-
 def generate_exchange_pairs(data: dict):
     base = data["base"]
     rates = data["rates"]
